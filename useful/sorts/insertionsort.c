aba78e3ec7365e1c80419cecabb5464b8635c07c
Kaustubh Hiware <hiwarekaustubh@googlemail.com>
2016-03-11 15:40:10 +0530

    Basic  insertion sort

---
+++ useful/sorts/insertionsort.c
@@ -0,0 +1,37 @@
+/* insertion sort ascending order */
+ 
+#include <stdio.h>
+ 
+int main()
+{
+  int n, array[1000], c, d, t;
+ 
+  printf("+++ enter number of element : ");
+  scanf("%d", &n);
+ 
+  printf("+++ enter %d integers : ", n);
+ 
+  for (c = 0; c < n; c++) {
+    scanf("%d", &array[c]);
+  }
+ 
+  for (c = 1 ; c <= n - 1; c++) {
+    d = c;
+ 
+    while ( d > 0 && array[d] < array[d-1]) {
+      t          = array[d];
+      array[d]   = array[d-1];
+      array[d-1] = t;
+ 
+      d--;
+    }
+  }
+ 
+  printf("+++ sorted list in ascending order : ");
+ 
+  for (c = 0; c <= n - 1; c++) {
+    printf("%d\n", array[c]);
+  }
+ 
+  return 0;
+}
